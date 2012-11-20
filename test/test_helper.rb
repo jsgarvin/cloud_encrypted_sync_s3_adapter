3a49974acc85416f915f351c665ad661486d22ca
Jonathan Garvin <jon@5valleys.com>
2012-11-19 21:56:04 -0700

    cleanup test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,18 +13,10 @@ require 'cloud_encrypted_sync_s3_adapter'
 module CloudEncryptedSyncS3Adapter
   class ActiveSupport::TestCase
 
-    setup :preset_environment
-
-    def preset_environment
-      CloudEncryptedSync::Master.instance_variable_set(:@config,nil)
-      CloudEncryptedSync::Master.instance_variable_set(:@command_line_options, {
-        :data_dir => "#{Etc.getpwuid.dir}/.cloud_encrypted_sync"
-      })
-    end
-
     def adapter
       CloudEncryptedSync::Adapters::S3
     end
+
   end
 end
 require 'mocha'
